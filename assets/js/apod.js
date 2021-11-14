37e2d559aef94745a3d497c9aeb83eba2e051a99
Brandon Hoskins <branjames117@gmail.com>
2021-11-13 21:09:13 -0600

    added API call for NASA APOD

---
+++ assets/js/apod.js
@@ -0,0 +1,19 @@
+// Function to render to the DOM NASA's Astronomy Picture of the Day
+
+(function getAPOD() {
+  const API_URL = 'https://api.nasa.gov/planetary/apod?api_key=';
+  const API_KEY = 'lEF3XW7bbe3BIaacs2lmw47iySF6eR72wP6T1sin';
+
+  const apodEl = document.querySelector('.recent-card1 img');
+
+  fetch(API_URL + API_KEY).then((res) => {
+    if (res.ok) {
+      res.json().then((data) => {
+        // data object contains the following properties: copyright, data, explanation, hdurl, media_type, service_version, title, url
+        apodEl.src = data.url;
+      });
+    } else {
+      console.log('Error receiving data from API.');
+    }
+  });
+})();
