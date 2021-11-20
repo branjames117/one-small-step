1f120457b68239b254189a2f35ad7c900ec9be28
Brandon Hoskins <branjames117@gmail.com>
2021-11-19 21:55:22 -0600

    hardcoded HTML containers for APOD section

--- assets/js/apod.js
+++ assets/js/apod.js
@@ -5,71 +5,39 @@
   const api_key = 'lEF3XW7bbe3BIaacs2lmw47iySF6eR72wP6T1sin';
   const params = { params: { api_key } };
 
-  // create elements for displaying APOD
-  const sectionTitleEl = (document.createElement('h2').textContent =
-    'Image of the Day');
-  const titleEl = document.createElement('h3');
-  const linkEl = document.createElement('a');
-  const apodEl = document.createElement('img');
-  const avodEl = document.createElement('video');
-  const explanationEl = document.createElement('p');
-  const favoriteBtnEl = document.createElement('button');
-
-  // clear placeholder
-  clearSectionById('#apod-section');
-
-  // get section to display API data
-  const apodSectionEl = document.getElementById('apod-section');
-  apodSectionEl.style.position = 'relative';
-
   axios
     .get(url, params)
     .then((res) => {
       // set the title for the APOD
-      titleEl.textContent = `${res.data.title}`;
-      titleEl.style.fontSize = '2rem';
+      document.querySelector(
+        '#apod-section > h3'
+      ).textContent = `${res.data.title}`;
 
       // set the explanation
-      explanationEl.textContent = res.data.explanation;
-      explanationEl.classList = 'w-4/5 text-left';
-
-      // add a Favorite button, only for images
-      favoriteBtnEl.textContent = '☆';
-      favoriteBtnEl.style.position = 'absolute';
-      favoriteBtnEl.style.backgroundColor = 'black';
-      favoriteBtnEl.style.color = 'white';
-      favoriteBtnEl.style.top = '30px';
-      favoriteBtnEl.style.right = '150px';
-      favoriteBtnEl.style.borderRadius = '50%';
+      document.querySelector('#apod-section > p').textContent =
+        res.data.explanation;
 
       // check if APOD is an image or a video and append appropriate element to render it
       if (res.data.media_type === 'image') {
         // enclose img el in a link to HD url
-        linkEl.href = res.data.hdurl;
-        linkEl.appendChild(apodEl);
-        apodEl.src = res.data.url;
-        apodEl.title = `Click for HD version of ${res.data.title}.`;
+        document.querySelector('#apod-section > a').href = res.data.hdurl;
+        document.querySelector('#apod-section img').src = res.data.url;
+        document.querySelector(
+          '#apod-section img'
+        ).title = `Click for HD version of ${res.data.title}.`;
 
         // add event listener to Favorite button
-        favoriteBtnEl.imageObj = {
+        document.querySelector('#apod-section > button').imageObj = {
           title: res.data.title,
           thumbnail: res.data.url,
           hdUrl: res.data.hdurl,
         };
-        favoriteBtnEl.addEventListener('click', handleAddFavorite);
-
-        // render everything to DOM
-        apodSectionEl.append(
-          sectionTitleEl,
-          linkEl,
-          titleEl,
-          explanationEl,
-          favoriteBtnEl
-        );
+        document
+          .querySelector('#apod-section > button')
+          .addEventListener('click', handleAddFavorite);
       } else if (res.data.media_type === 'video') {
         // if video, append a video el instead of an img el
-        avodEl.src = res.data.url;
-        apodSectionEl.append(titleEl, avodEl, explanationEl);
+        document.querySelector('#apod-section > video').src = res.data.url;
       }
     })
     // TO DO - enhance user feedback for API error with a modal
