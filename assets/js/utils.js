df5d3ab4d58d0b9fa8e1d20db8deb0705ab0604d
Brandon Hoskins <branjames117@gmail.com>
2021-11-24 21:01:22 -0600

    individual images now render to their own section, still need to add favoriting functionality to these

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,9 +1,9 @@
 // function to render specific section based on which nav bar link is clicked
+
 console.log('Running utils');
 function renderSection(sectionId) {
   // grab all sections and convert nodelist to array
   const sections = Array.from(document.querySelectorAll('section'));
-  console.log('Rendering ', sectionId);
   sections.forEach((section) => {
     if (section.id === sectionId) {
       section.classList.remove('hidden');
@@ -14,9 +14,11 @@ function renderSection(sectionId) {
 }
 
 // Immediately render ISS tracker section on page load
+
 renderSection('iss-tracker-section');
 
 // Add event listeners to nav bar links
+
 const navbarLinks = Array.from(document.querySelectorAll('#navbar-links li'));
 navbarLinks.forEach((link) => {
   link.addEventListener('click', (e) =>
@@ -33,25 +35,66 @@ async function getImageFromManifest(imageObj) {
 
   // Axios trick to store the data returned from the get request in url variable
   const promise = axios.get(imageObj.manifest);
-  const url = await promise.then((res) => res.data[0]);
-  console.log(title);
-  console.log(description);
-  console.log(url);
+  const url = await promise.then((res) => {
+    // most browsers don't support rendering TIFF files so if the first item in the manifest happens to be one, skip to the next
+    if (res.data[0].includes('.tif')) {
+      return res.data[1];
+    } else {
+      return res.data[0];
+    }
+  });
+  document.querySelector('#hd-title span').textContent = title;
+  document.querySelector('#hd-desc').textContent = description;
+  document.querySelector('#hd-img').src = url;
+  renderSection('hd-section');
 }
 
 // Function to grab the url from the Astronomy Picture of the Day
+
 function getImageFromURL(imageObj) {
   // To do: open the URL in the modal
   const title = imageObj.title;
   const description = imageObj.description || imageObj.explanation;
   const url = imageObj.url;
 
-  console.log(title);
-  console.log(description);
-  console.log(url);
+  renderSection('hd-section');
+  document.querySelector('#hd-title span').textContent = title;
+  document.querySelector('#hd-desc').textContent = description;
+  document.querySelector('#hd-img').src = url;
+}
+
+// Variables and functions related to the modal
+
+const overlay = document.querySelector('#modal-overlay');
+overlay.addEventListener('click', toggleModal);
+
+var closeModal = document.querySelector('#modal-close');
+closeModal.addEventListener('click', toggleModal);
+
+document.onkeydown = function (event) {
+  event = event || window.event;
+  var isEscape = false;
+  if ('key' in event) {
+    isEscape = event.key === 'Escape' || event.key === 'Esc';
+  } else {
+    isEscape = event.keyCode === 27;
+  }
+  if (isEscape && document.body.classList.contains('modal-active')) {
+    toggleModal();
+  }
+};
+
+function toggleModal() {
+  console.log('modal toggled');
+  const body = document.querySelector('body');
+  const modal = document.querySelector('#modal');
+  modal.classList.toggle('opacity-0');
+  modal.classList.toggle('pointer-events-none');
+  body.classList.toggle('modal-active');
 }
 
 // function to grab the local storage object
+
 function grabLocalStorage() {
   return JSON.parse(localStorage.userInfo);
 }
