40ef1ce72eb922db02f22fa6a5c410ac00bcfa32
Brandon Hoskins <branjames117@gmail.com>
2021-11-24 21:54:34 -0600

    removed console.log

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -61,7 +61,6 @@ function populateFavorites() {
       document.querySelector(
         `#favorite-container-${idx + 1} h3 a`
       ).style.cursor = 'pointer';
-      console.log(favorite);
 
       // remove from favorites button
       document.querySelector(
