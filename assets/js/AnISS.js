38b0a3b4d9cf8b54453b0163189dc3aa56808155
pmnni87@gmail.com <91038339+pmanning901@users.noreply.github.com>
2021-11-27 17:30:30 -0600

    add all updates from AnISS.js and index.html

--- assets/js/AnISS.js
+++ assets/js/AnISS.js
@@ -1,20 +1,8 @@
+const astroInSpace = res.data.people;
+const indvOnISS = 
+astroInSpace.filter((person) => person.craft === 'ISS');
 
 
-function addListItem (name, craftId){
-    const astronames = document.querySelector('name')
-    const astroInIss = objectFromApi.people.filter(person =>{ person.craft==='ISS'})
-    li.appendChild(document.createElement(names))
-    ul.appendChild(li)
-    }
-    function afterload(){
-        var data = JSON.parse(this.responseText);
-        var name = document.createElement('astroName');
-    
-        const results = data.results;
-    
-        //loop through items
-        results.array.forEach(item => {
-            addListItem(item.name, "names"); 
-        });
-    }
-    
+axios.get('http://api.open-notify.org/astros.json')
+    .then(res => console.log(
+        res.data.people.filter(person => person.craft === 'ISS')));
